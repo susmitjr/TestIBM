5e4945a50fa0cdd1607d4a9eac382f0d25f07b84
SusmitJana <SusmitJana@DESKTOP-6ICKLCA>
2020-01-22 22:53:57 +0530

    First Commit

---
+++ IBM-cucumber-demo/src/test/java/com/IBM/RunMaven/RunCukeTest.java
@@ -0,0 +1,13 @@
+package com.IBM.RunMaven;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"src/main/resources"})
+
+public class RunCukeTest {
+
+}
---
+++ IBM-cucumber-demo/src/test/java/com/IBM/stepDefs/DemoDefination.java
@@ -0,0 +1,29 @@
+package com.IBM.stepDefs;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DemoDefination {
+	@Given("^I have a configured Cucumber-jvm project$")
+	public void i_have_a_configured_Cucumber_jvm_project() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I run it within my IDE$")
+	public void i_run_it_within_my_IDE() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I will be able to run connected step definitions$")
+	public void i_will_be_able_to_run_connected_step_definitions() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	
+
+}
